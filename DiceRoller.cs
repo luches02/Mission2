e5b5d16b108425f76e1c7b1a5fa3a248c911a0ce
student <student@OLUCHIOCHULDA9B>
2024-01-17 18:59:51 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{ 
+    class DiceRoller
+    {
+        private Random random;
+
+        public DiceRoller()
+        {
+            random = new Random();
+        }
+
+        public int[] SimulateRolls(int numberOfRolls)
+        {
+            int[] results = new int[13];
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int roll1 = random.Next(1, 7);
+                int roll2 = random.Next(1, 7);
+                int sum = roll1 + roll2;
+
+                results[sum]++;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+namespace ConsoleApp1
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            int numberOfRolls = int.Parse(Console.ReadLine());
+
+            DiceRoller diceRoller = new DiceRoller();
+            int[] results = diceRoller.SimulateRolls(numberOfRolls);
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numberOfRolls);
+
+            PrintHistogram(results);
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+
+        static void PrintHistogram(int[] results)
+        {
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write(i + ": ");
+
+                int percentage = results[i] * 100 / results.Length;
+                int asterisks = percentage / 1;
+
+                for (int j = 0; j < asterisks; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
